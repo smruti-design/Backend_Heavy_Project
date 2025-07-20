1fe25fc08be81c91f65a5d34f194d1c5bd2676f5
Smruti Sourav Sahoo <smruti.sahoo@SMRUTI-SAHOO-MAC.local>
2025-07-21 00:19:31 +0530

    added utils

--- app/src/main/java/ticket/booking/services/UserBookingService.java
+++ app/src/main/java/ticket/booking/services/UserBookingService.java
@@ -3,10 +3,12 @@ package ticket.booking.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ticket.booking.entities.User;
+import ticket.booking.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
 
@@ -16,11 +18,34 @@ public class UserBookingService {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    private static final String USERS_PATH = "../localDB/users.json" ;
+    private static final String USER_FILE_PATH = "../localDB/users.json" ;
 
     public UserBookingService(User user) throws IOException {
         this.user = user;
-        File users = new File(USERS_PATH);
+        File users = new File(USER_FILE_PATH);
         userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
     }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File usersFile = new File(USER_FILE_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
 }
